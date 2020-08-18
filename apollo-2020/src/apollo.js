47475d7a4172bf169fbbbe09669d61b4e0d96c70
백호동 <ghehd2311@gmail.com>
2020-08-18 18:29:37 +0900

    movieql API 연동 테스트

--- apollo-2020/src/apollo.js
+++ apollo-2020/src/apollo.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
   //uri: 'https://movieql.now.sh/',//노마드 코더 movieql 백단 url
-  uri: 'localhost:4000', //movieql 켜야함
+  uri: 'http://localhost:4000/', //movieql 켜야함
 });
 
 export default client;
