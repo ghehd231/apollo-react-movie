9781cd6a7f3ae5fa7415a2da355cf88b5a26fbae
백호동 <ghehd2311@gmail.com>
2020-08-19 17:44:34 +0900

    Home 화면 퍼블리싱 및 Detail.js에서 영화 상세 불러오는 API연동

--- apollo-2020/src/routes/Detail.js
+++ apollo-2020/src/routes/Detail.js
@@ -1,1 +1,34 @@
-export default () => 'Detail';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    #apollo를 위한 부분
+    movie(id: $id) {
+      #graphql서버로 query를 보내기 위한 부분
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  //const {id} = useParams(); // 이 부분에서 타입문제(param의 id가 string) 때문에 400에러 나서 바꿔줌
+  const param = useParams();
+  const id = Number(param.id);
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id }, //GET_MOVIE에서 변수 id가 있어야 함으로 변수를 지정해준다
+  });
+
+  if (loading) {
+    return 'loading...';
+  }
+
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
