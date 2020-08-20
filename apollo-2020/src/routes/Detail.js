909afbf5ac5cdb47d56ab4271c157df6b01f8af1
백호동 <ghehd2311@gmail.com>
2020-08-20 18:05:48 +0900

    Home, Detail, Movie 퍼블리싱 수정

--- apollo-2020/src/routes/Detail.js
+++ apollo-2020/src/routes/Detail.js
@@ -2,18 +2,59 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     #apollo를 위한 부분
     movie(id: $id) {
       #graphql서버로 query를 보내기 위한 부분
-      id
       title
       medium_cover_image
+      language
+      rating
       description_intro
     }
+   
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+    margin-left: 10px;
+    width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+    font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image : url(${props => props.bg});
+  background-size: cover;
+  background-position: center center;
 `;
 
 export default () => {
@@ -24,11 +65,27 @@ export default () => {
     variables: { id }, //GET_MOVIE에서 변수 id가 있어야 함으로 변수를 지정해준다
   });
 
-  if (loading) {
-    return 'loading...';
-  }
+  //   if (loading) {
+  //     return 'loading...';
+  //   }
 
-  if (data && data.movie) {
-    return data.movie.title;
-  }
+  //   if (data && data.movie) {
+  //     return data.movie.title;
+  //   }
+  return (
+    <Container>
+      <Column>
+        <Title>
+          {loading ? 'Loading...' : data.movie.title}
+        </Title>
+        {!loading && data.movie && (
+            <>
+                <Subtitle>{data.movie.language} · {data.movie.rating}</Subtitle>
+                <Description>{data.movie.description_intro}</Description>
+            </>
+        )}
+      </Column>
+      <Poster bg={data && data.movie ? data.movie.medium_cover_image : ""} ></Poster>
+    </Container>
+  );
 };
